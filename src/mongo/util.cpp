c5ee9cfdca871af77293bba97767761f44bcccd5
Matías Pavez <matias.pavez.b@gmail.com>
2018-07-04 00:51:40 -0400

    bugfix when generating json from empty array

--- src/mongo/util.cpp
+++ src/mongo/util.cpp
@@ -4,6 +4,7 @@ namespace ltm_db_mongo {
     namespace util {
 
         std::string vector_to_str(const std::vector<std::string> &array) {
+            if (array.empty()) return "[]";
             std::vector<std::string>::const_iterator it;
             std::stringstream ss;
             ss << "[";
@@ -16,6 +17,7 @@ namespace ltm_db_mongo {
         }
 
         std::string vector_to_str(const std::vector<int> &array) {
+            if (array.empty()) return "[]";
             std::vector<int>::const_iterator it;
             std::ostringstream ss;
             ss << "[";
@@ -28,6 +30,7 @@ namespace ltm_db_mongo {
         }
 
         std::string vector_to_str(const std::vector<uint32_t> &array) {
+            if (array.empty()) return "[]";
             std::vector<uint32_t>::const_iterator it;
             std::ostringstream ss;
             ss << "[";
